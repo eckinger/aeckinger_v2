de98bb1fb5cad206218e3feba9507b34cf6f02e5
Alexander Eckinger <eckinger@Alexanders-MacBook-Air.local>
2025-02-20 09:11:17 -0600

    configed local file storage and created photos table

--- Config/Config.hs
+++ Config/Config.hs
@@ -4,11 +4,16 @@ import IHP.Prelude
 import IHP.Environment
 import IHP.FrameworkConfig
 import Web.View.CustomCSSFramework
+import IHP.FileStorage.Config
 
 config :: ConfigBuilder
 config = do
     -- See https://ihp.digitallyinduced.com/Guide/config.html
     -- for what you can do here
+    option Development
+    option (AppHostname "localhost")
+
+    initStaticDirStorage
 
     -- Tailwind
     option customTailwind
