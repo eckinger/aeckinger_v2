3b40824bd2de292403059351e57d66a400ec133d
Alexander Eckinger <eckinger@Alexanders-MacBook-Air.local>
2025-02-20 18:06:11 -0600

    got some sort of smart reordering working, but it orders weirdly

--- Web/View/Photos/Index.hs
+++ Web/View/Photos/Index.hs
@@ -8,22 +8,20 @@ instance View IndexView where
         setLayout indexPhotosLayout
 
     html IndexView { .. } = [hsx|
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <tbody>{forEach photos renderPhoto}</tbody>
+        <div class="columns-1 md:columns-2 lg:columns-3 gap-4 space-y-4 p-8">
+            {forEach photos renderPhoto}
         </div>
     |]
 
 renderPhoto :: Photo -> Html
 renderPhoto photo = [hsx|
-    <div class="overflow-hidden h-full w-full">
-            <a href={photo.photoUrl} data-fancybox="gallery">
-                <img src={photo.photoUrl}
-                     alt={photo.caption}
-                     class="block object-cover object-center opacity-0 animate-fade-in transition duration-500 transform scale-100 hover:scale-110">
-            </a>
+    <div class="overflow-hidden h-min w-full">
+        <a href={photo.photoUrl} data-fancybox="gallery">
+            <img src={photo.photoUrl}
+                 alt={photo.caption}
+                 class="block object-cover object-center opacity-0 animate-fade-in transition duration-500 transform scale-100 hover:scale-110">
+        </a>
     </div>
-
-
 
     <!--
     <tr>
